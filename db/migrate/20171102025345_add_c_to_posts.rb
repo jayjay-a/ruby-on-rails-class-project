f5c859435eecfe2d70d3d7a81db0dd70b7e4bf51
jmagno3 <agno.julius@gmail.com>
2017-11-01 21:54:23 -0500

    add message column to posts

---
+++ db/migrate/20171102025345_add_c_to_posts.rb
@@ -0,0 +1,5 @@
+class AddCToPosts < ActiveRecord::Migration[5.1]
+  def change
+    add_column :posts, :message, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026035501) do
+ActiveRecord::Schema.define(version: 20171102025345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20171026035501) do
     t.integer "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "message"
   end
 
   create_table "user_groups", force: :cascade do |t|
