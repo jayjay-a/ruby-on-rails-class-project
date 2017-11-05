b075eb5619a883b10d0efdf64725efd7bda22017
Julius Agno <agno.julius@gmail.com>
2017-11-05 01:19:14 -0500

    implement import from csv, commented out nonfunctional export

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,24 @@ class User < ApplicationRecord
   has_many :user_roles
   has_many :user_groups
   has_many :groups, :through => :user_groups
+  require 'csv'
+
+  #def self.to_csv(fields=column_names, options={})
+  #CSV.generate(options) do |csv|
+  #  csv << fields
+  #    all.each do |user|
+  #      csv << user.attributes.values_at(*fields)
+  #    end
+  #  end
+  #end
+
+  def self.import(file)
+    CSV.foreach(file.path, headers:true) do |row|
+      users_hash = row.to_hash
+      user = find_or_create_by!(fname: users_hash['fname'])
+      user.update_attributes(users_hash)
+    end
+  end
 
 
 end
