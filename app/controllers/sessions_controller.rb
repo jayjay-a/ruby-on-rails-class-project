5376079537c79c0cca84fa5e6a4f78f263242b74
julius agno <agno.julius@gmail.com>
2017-11-21 13:45:30 -0600

    simple flash notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in"
     else
-      flash[:alert] = 'Invalid username or password'
+      flash[:notice] = 'Invalid username or password'
       render "new"
     end
   end
