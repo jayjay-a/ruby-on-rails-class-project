f032611c4782d423c95accf2de33991c9b459477
julius agno <agno.julius@gmail.com>
2017-11-25 02:08:48 -0600

    fix csv AGAIN goddamn

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,15 +15,21 @@ class User < ApplicationRecord
   #import csv
   require 'csv'
   def self.import(file)
-    CSV.foreach(file.path, headers:true) do |row|
-      users_hash = row.to_hash
-      user = find_or_create_by!(fname: users_hash['fname'])
-      user.update_attributes(users_hash)
-    end
-  end
+    CSV.foreach(file.path, headers: true) do |row|
 
+      user_hash = row.to_hash # exclude the price field
+      user = User.where(fname: user_hash["fname"])
+
+      if user.count == 1
+        user.first.update_attributes(user_hash)
+      else
+        User.create!(user_hash)
+      end # end if statemnet
+    end # end foreach
+  end # end method
 
 end
+
 def formatted_name
   "#{fname} #{lname}"
 end
