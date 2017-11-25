41de575dac8474060ee0e88693f5fe330ff94286
julius agno <agno.julius@gmail.com>
2017-11-25 00:48:43 -0600

    create way to log out user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,4 +12,9 @@ class SessionsController < ApplicationController
       render "new"
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to channels_path, notice: "Logged out"
+  end
 end
