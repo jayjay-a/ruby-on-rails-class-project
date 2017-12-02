8eba84b40ed44222a787c548bb20ff84b2d7ec86
julius agno <agno.julius@gmail.com>
2017-12-02 00:51:28 -0600

    clean up flash notices they look SICK

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to channels_path, notice: "Logged in"
+      redirect_to channels_path, flash: { success: "Logged in" }
     else
-      flash[:notice] = 'Invalid username or password'
+      flash[:danger] = 'Invalid username or password'
       render "new"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to channels_path, notice: "Logged out"
+    redirect_to channels_path, flash: { success: "Logged out" }
+    
   end
 end
