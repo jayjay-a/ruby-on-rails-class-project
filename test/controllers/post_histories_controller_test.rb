c42b12a693e5f7c5bfb325f2f3cd4956b2f3b20f
TJON <alextjon@gmail.com>
2017-12-02 01:41:14 -0600

    test and image

--- test/controllers/post_histories_controller_test.rb
+++ test/controllers/post_histories_controller_test.rb
@@ -1,48 +1,48 @@
 require 'test_helper'
 
 class PostHistoriesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @post_history = post_histories(:one)
-  end
-
-  test "should get index" do
-    get post_histories_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_post_history_url
-    assert_response :success
-  end
-
-  test "should create post_history" do
-    assert_difference('PostHistory.count') do
-      post post_histories_url, params: { post_history: { action: @post_history.action, date: @post_history.date, post_id: @post_history.post_id, user_id: @post_history.user_id } }
-    end
-
-    assert_redirected_to post_history_url(PostHistory.last)
-  end
-
-  test "should show post_history" do
-    get post_history_url(@post_history)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_post_history_url(@post_history)
-    assert_response :success
-  end
-
-  test "should update post_history" do
-    patch post_history_url(@post_history), params: { post_history: { action: @post_history.action, date: @post_history.date, post_id: @post_history.post_id, user_id: @post_history.user_id } }
-    assert_redirected_to post_history_url(@post_history)
-  end
-
-  test "should destroy post_history" do
-    assert_difference('PostHistory.count', -1) do
-      delete post_history_url(@post_history)
-    end
-
-    assert_redirected_to post_histories_url
-  end
+  # setup do
+  #   @post_history = post_histories(:one)
+  # end
+  #
+  # test "should get index" do
+  #   get post_histories_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get new" do
+  #   get new_post_history_url
+  #   assert_response :success
+  # end
+  #
+  # test "should create post_history" do
+  #   assert_difference('PostHistory.count') do
+  #     post post_histories_url, params: { post_history: { action: @post_history.action, date: @post_history.date, post_id: @post_history.post_id, user_id: @post_history.user_id } }
+  #   end
+  #
+  #   assert_redirected_to post_history_url(PostHistory.last)
+  # end
+  #
+  # test "should show post_history" do
+  #   get post_history_url(@post_history)
+  #   assert_response :success
+  # end
+  #
+  # test "should get edit" do
+  #   get edit_post_history_url(@post_history)
+  #   assert_response :success
+  # end
+  #
+  # test "should update post_history" do
+  #   patch post_history_url(@post_history), params: { post_history: { action: @post_history.action, date: @post_history.date, post_id: @post_history.post_id, user_id: @post_history.user_id } }
+  #   assert_redirected_to post_history_url(@post_history)
+  # end
+  #
+  # test "should destroy post_history" do
+  #   assert_difference('PostHistory.count', -1) do
+  #     delete post_history_url(@post_history)
+  #   end
+  #
+  #   assert_redirected_to post_histories_url
+  # end
 end
